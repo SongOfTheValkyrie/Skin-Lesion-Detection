fc2878acaaad0753bb3696c10b80d8b81308f678
Adrian Gheorghiu <adriangheorghiu00@gmail.com>
2023-01-20 19:33:58 +0100

    Added OOD detection to app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -28,17 +28,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Fragment;
 
+import org.pytorch.Device;
 import org.pytorch.IValue;
 import org.pytorch.Module;
 import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements ImageReader.OnImageAvailableListener {
@@ -52,7 +57,8 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
     TextView resultText;
 
     Uri imageUri;
-    Module module;
+    Module module_feature;
+    Module module_classify;
     Bitmap curImage = null;
     int rectHeight;
     int rectWidth;
@@ -159,6 +165,98 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
         }
     }
 
+    public Tensor getCurImageTensor() {
+        int w = curImage.getWidth();
+        int h = curImage.getHeight();
+        int startX = (w / 2 - rectWidth / 2);
+        int startY = (h / 2 - rectHeight / 2);
+        Bitmap centerPart = Bitmap.createBitmap(curImage, startX, startY, rectWidth, rectHeight);
+        Matrix m = new Matrix();
+        m.postScale(1.f / resizeRatio, 1.f / resizeRatio);
+        Bitmap resized = Bitmap.createBitmap(centerPart, 0, 0, rectWidth, rectHeight, m, false);
+        centerPart.recycle();
+        return TensorImageUtils.bitmapToFloat32Tensor(resized,
+                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+    }
+
+    public float[][] readMatrix(String path) {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(path))) {
+            int n = Integer.parseInt(fileReader.readLine());
+            int m = Integer.parseInt(fileReader.readLine());
+            float[][] data = new float[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++)
+                    data[i][j] = Float.parseFloat(fileReader.readLine());
+            }
+            return data;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public float[][] matrixMul(float[][] m1, float[][] m2) {
+        if (m1[0].length != m2.length)
+            throw new RuntimeException();
+
+        float[][] res = new float[m1.length][m2[0].length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m2[0].length; j++)
+                res[i][j] = 0.f;
+        }
+
+        for (int k = 0; k < m1[0].length; k++) {
+            for (int i = 0; i < m1.length; i++) {
+                for (int j = 0; j < m2[0].length; j++)
+                    res[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+        return res;
+    }
+
+    public float[][] matrixDif(float[][] m1, float[][] m2) {
+        if (m1.length != m2.length || m1[0].length != m2[0].length)
+            throw new RuntimeException();
+
+        float[][] res = new float[m1.length][m1[0].length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++)
+                res[i][j] = m1[i][j] - m2[i][j];
+        }
+        return res;
+    }
+
+    public float[][] matrixTranspose(float[][] m) {
+        float[][] res = new float[m[0].length][m.length];
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++)
+                res[j][i] = m[i][j];
+        }
+        return res;
+    }
+    public float mahalanobis_distance(float[][] feature_map, float[][] mean_feature_map, float[][] covar_inverse) {
+        float[][] adjusted_feature_map = matrixDif(feature_map, mean_feature_map);
+        return matrixMul(matrixMul(adjusted_feature_map, covar_inverse), matrixTranspose(adjusted_feature_map))[0][0];
+    }
+
+    public float[][] vecToMatrix(float[] v) {
+        float[][] res = new float[1][v.length];
+        System.arraycopy(v, 0, res[0], 0, v.length);
+        return res;
+    }
+
+    public float rmd_confidence(float[][] feature_map, float[][] mean_feature_maps, float[][] mean_feature_map_0, float[][] covar_inverse, float[][] covar_0_inverse) {
+        float md_0 = mahalanobis_distance(feature_map, mean_feature_map_0, covar_0_inverse);
+
+        ArrayList<Float> rmd = new ArrayList<>();
+        for (float[] mean_feature_map : mean_feature_maps)
+            rmd.add(mahalanobis_distance(feature_map, vecToMatrix(mean_feature_map), covar_inverse) - md_0);
+
+        float min_val = rmd.get(0);
+        for (int i = 1; i < rmd.size(); i++)
+            min_val = rmd.get(i) < min_val ? rmd.get(i) : min_val;
+        return -min_val;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -169,8 +267,12 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
         loadImage = (Button)findViewById(R.id.button);
         detect = (Button)findViewById(R.id.detect);
         resultText = (TextView)findViewById(R.id.result_text);
-
-        module = Module.load(assetFilePath("ham10k_optimized.pt"));
+        module_feature = Module.load(assetFilePath("ham10k_feature_mobile.pt"));
+        module_classify = Module.load(assetFilePath("ham10k_classify_mobile.pt"));
+        float[][] mean_feature_map_0 = readMatrix(assetFilePath("mean_feature_map_0.matrix"));
+        float[][] mean_feature_maps = readMatrix(assetFilePath("mean_feature_maps.matrix"));
+        float[][] covar_0_inverse = readMatrix(assetFilePath("covar_0_inverse.matrix"));
+        float[][] covar_inverse = readMatrix(assetFilePath("covar_inverse.matrix"));
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
             openCamera();
@@ -189,6 +291,15 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
             resultText.setText("");
             if (!imageCaptured) {
                 loadImage.setText("Take Another Image");
+                Thread t = new Thread(() -> {
+                    Tensor inputTensor = getCurImageTensor();
+                    Tensor feature = module_feature.forward(IValue.from(inputTensor)).toTensor();
+                    float confidence = rmd_confidence(vecToMatrix(feature.getDataAsFloatArray()), mean_feature_maps, mean_feature_map_0, covar_inverse, covar_0_inverse);
+
+                    if (imageCaptured)
+                        resultText.setText("ID confidence score: "  + String.format("%.02f", confidence));
+                });
+                t.start();
             } else {
                 loadImage.setText("Capture Image");
             }
@@ -197,21 +308,14 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
 
         detect.setOnClickListener(view -> {
             if (curImage != null && imageCaptured) {
-                Thread t = new Thread(() -> {int h = curImage.getHeight();
-                    int w = curImage.getWidth();
-                    int startX = (w / 2 - rectWidth / 2);
-                    int startY = (h / 2 - rectHeight / 2);
-                    Bitmap centerPart = Bitmap.createBitmap(curImage, startX, startY, rectWidth, rectHeight);
-                    Matrix m = new Matrix();
-                    m.postScale(1.f / resizeRatio, 1.f / resizeRatio);
-                    Bitmap resized = Bitmap.createBitmap(centerPart, 0, 0, rectWidth, rectHeight, m, false);
-                    centerPart.recycle();
-                    Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resized,
-                            TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+                Thread t = new Thread(() -> {
+                    Tensor inputTensor = getCurImageTensor();
                     runOnUiThread(() -> {
                         resultText.setText("Analyzing...");
                     });
-                    float[] out = module.forward(IValue.from(inputTensor)).toTensor().getDataAsFloatArray();
+                    Tensor feature = module_feature.forward(IValue.from(inputTensor)).toTensor();
+                    System.out.println(Arrays.toString(feature.shape()));
+                    float[] out = module_classify.forward(IValue.from(feature)).toTensor().getDataAsFloatArray();
                     System.out.println("Result vector is: " + Arrays.toString(out));
                     int maxPos = 0;
                     float maxVal = 0;
